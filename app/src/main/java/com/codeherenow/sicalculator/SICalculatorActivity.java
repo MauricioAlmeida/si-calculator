ce5b28dbbe992575960fae2830b4fe2ea551489b
Mauricio Almeida <mauricio.almeida@uol.com.br>
2018-01-04 07:42:03 -0200

    Mapped UI components

--- app/src/main/java/com/codeherenow/sicalculator/SICalculatorActivity.java
+++ app/src/main/java/com/codeherenow/sicalculator/SICalculatorActivity.java
@@ -15,13 +15,32 @@ package com.codeherenow.sicalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SICalculatorActivity extends Activity {
+
+	private EditText editPrincipalAmount;
+	private EditText editInterestRate;
+	private TextView textPeriod;
+	private TextView textResult;
+	private Button buttonCalculate;
+	private SeekBar seekPeriod;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sicalculator);
+
+		editPrincipalAmount = (EditText) findViewById(R.id.edt_principal_amount);
+		editInterestRate = (EditText) findViewById(R.id.edt_interest_rate);
+		textPeriod = (TextView) findViewById(R.id.seek_period_text);
+		textResult = (TextView) findViewById(R.id.txt_result);
+		buttonCalculate = (Button) findViewById(R.id.button_calculate);
+		seekPeriod = (SeekBar) findViewById(R.id.seek_period);
+
 	}
 
 }
